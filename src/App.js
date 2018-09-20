b676a73992a28970423275423f6eb61599c81929
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2018-09-20 00:19:53 -0300

    i18n: added and first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { I18nextProvider } from 'react-i18next'
 import { MuiThemeProvider } from 'material-ui/styles'
 
-import AppRouter from './router'
 import './setup'
+import AppRouter from './router'
 import NotificationMessage from './components/NotificationMessage'
 import theme from './materializeTheme'
 import reduxStore from './redux'
+import i18n from './i18n'
 
 const App = () => {
   return (
     <Provider store={reduxStore}>
-      <MuiThemeProvider theme={theme}>
-        <NotificationMessage />
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </MuiThemeProvider>
+      <I18nextProvider i18n={i18n}>
+        <MuiThemeProvider theme={theme}>
+          <NotificationMessage />
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </I18nextProvider>
     </Provider>
   )
 }
